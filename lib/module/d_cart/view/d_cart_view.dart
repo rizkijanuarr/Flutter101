9264bb6ad17d5c715142ba5808eaeb0a1b503c3b
rizkijanuarr <72480809+rizkijanuarr@users.noreply.github.com>
2024-08-09 00:53:36 +0700

    D. Cart (done)

--- lib/module/d_cart/view/d_cart_view.dart
+++ lib/module/d_cart/view/d_cart_view.dart
@@ -3,21 +3,89 @@ import 'package:flutter101/core.dart';
 import '../controller/d_cart_controller.dart';
 
 class DCartView extends StatefulWidget {
-  const DCartView({Key? key}) : super(key: key);
+  const DCartView({super.key});
 
   Widget build(context, DCartController controller) {
     controller.view = this;
     return Scaffold(
       appBar: AppBar(
-        title: const Text("DCart"),
+        title: const Text("Cart"),
         actions: const [],
       ),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: const [],
-          ),
+      body: Container(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: controller.products.length,
+                shrinkWrap: true,
+                padding: EdgeInsets.zero,
+                clipBehavior: Clip.none,
+                itemBuilder: (context, index) {
+                  var item = controller.products[index];
+                  item["qty"] ??= 0;
+                  return Card(
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        backgroundColor: Colors.grey[200],
+                        backgroundImage: NetworkImage(
+                          item["photo"],
+                        ),
+                      ),
+                      title: Text(item["product_name"]),
+                      subtitle: Text("${item["price"]} USD"),
+                      trailing: SizedBox(
+                        width: 120.0,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            CircleAvatar(
+                              backgroundColor: Colors.blueGrey,
+                              radius: 12.0,
+                              child: Center(
+                                child: IconButton(
+                                  onPressed: () => controller.decreaseQty(item),
+                                  icon: const Icon(
+                                    Icons.remove,
+                                    color: Colors.white,
+                                    size: 9.0,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                "${item["qty"]}",
+                                style: const TextStyle(
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ),
+                            CircleAvatar(
+                              backgroundColor: Colors.blueGrey,
+                              radius: 12.0,
+                              child: Center(
+                                child: IconButton(
+                                  onPressed: () => controller.increaseQty(item),
+                                  icon: const Icon(
+                                    Icons.add,
+                                    color: Colors.white,
+                                    size: 9.0,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
