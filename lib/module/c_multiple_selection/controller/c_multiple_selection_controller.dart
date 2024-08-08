47fdea77393e91e156898b5214d352f9c5239561
rizkijanuarr <72480809+rizkijanuarr@users.noreply.github.com>
2024-08-09 00:39:38 +0700

    C. Multiple Selection (done)

--- lib/module/c_multiple_selection/controller/c_multiple_selection_controller.dart
+++ lib/module/c_multiple_selection/controller/c_multiple_selection_controller.dart
@@ -22,4 +22,78 @@ class CMultipleSelectionController extends State<CMultipleSelectionView> {
 
   @override
   Widget build(BuildContext context) => widget.build(context, this);
+
+  // Pertama kita buat datanya dulu yaa!
+  List<Map<String, dynamic>> categories = [
+    {
+      "label": "Aan",
+    },
+    {
+      "label": "Citra",
+    },
+    {
+      "label": "Lestari",
+    },
+    {
+      "label": "Aziz",
+    },
+    {
+      "label": "Agung",
+    },
+    {
+      "label": "Nabila",
+    },
+    {
+      "label": "Tasim",
+    },
+    {
+      "label": "Kaleann semua keren!",
+    },
+    {
+      "label": "Good game bro dimanapun berada!",
+    }
+  ];
+
+  // Kedua kita buat warnanya ketika di select ya!
+  updateIndex(int newIndex) {
+    if (categories[newIndex]["selected"] == true) {
+      categories[newIndex]["selected"] = false;
+    } else {
+      categories[newIndex]["selected"] = true;
+    }
+    setState(() {});
+  }
+
+  // save apa yang di selected
+  List getValues() {
+    return categories.where((i) => i["selected"] == true).toList();
+  }
+
+  doSave() {
+    var selectedValues = getValues();
+
+    // Tampilkan di console
+    print(selectedValues);
+
+    // Tampilkan di ui melalui dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Result Multiple Select"),
+          content: Text(selectedValues.isNotEmpty
+              ? selectedValues.map((e) => e["label"]).join("\n")
+              : "No values selected."),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text("OK"),
+            ),
+          ],
+        );
+      },
+    );
+  }
 }
--- lib/module/c_multiple_selection/view/c_multiple_selection_view.dart
+++ lib/module/c_multiple_selection/view/c_multiple_selection_view.dart
@@ -3,21 +3,60 @@ import 'package:flutter101/core.dart';
 import '../controller/c_multiple_selection_controller.dart';
 
 class CMultipleSelectionView extends StatefulWidget {
-  const CMultipleSelectionView({Key? key}) : super(key: key);
+  const CMultipleSelectionView({super.key});
 
   Widget build(context, CMultipleSelectionController controller) {
     controller.view = this;
     return Scaffold(
       appBar: AppBar(
-        title: const Text("CMultipleSelection"),
+        title: const Text("Multiple Selection"),
         actions: const [],
       ),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: const [],
-          ),
+      body: Container(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            // Implementasi Datanya!
+            Expanded(
+              child: ListView.builder(
+                // data
+                itemCount: controller.categories.length,
+                shrinkWrap: true,
+                padding: EdgeInsets.zero,
+                clipBehavior: Clip.none,
+                itemBuilder: (context, index) {
+                  // data
+                  var item = controller.categories[index];
+                  // warna
+                  bool selected = item["selected"] == true;
+                  return Card(
+                    // warna
+                    color: selected ? Colors.redAccent : Colors.white,
+                    child: ListTile(
+                      // warna
+                      onTap: () => controller.updateIndex(index),
+                      title: Text(
+                        "${item["label"]}",
+                        // warna
+                        style: TextStyle(
+                          color: selected ? Colors.white : Colors.black,
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            ElevatedButton.icon(
+              icon: const Icon(Icons.save),
+              label: const Text("Saved!"),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black,
+                foregroundColor: Colors.white,
+              ),
+              onPressed: () => controller.doSave(),
+            ),
+          ],
         ),
       ),
     );
