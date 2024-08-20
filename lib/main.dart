92c283accb9c53479b8c83280e7b29b4be240753
Rizki Januar I. <id.rizkijanuar@gmail.com>
2024-08-20 21:09:46 +0700

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,8 @@
 import 'package:flutter101/state_util.dart';
 import 'package:flutter101/core.dart';
 import 'package:flutter/material.dart';
-import 'package:window_manager/window_manager.dart';
 
 void main() async {
-  // SETUP WINDOWS
-  WidgetsFlutterBinding.ensureInitialized();
-  await windowManager.ensureInitialized();
-  await windowManager.setSize(const Size(360.0, 440.0));
-  // Always Top
-  windowManager.setAlwaysOnTop(true);
-
   runApp(const MyApp());
 }
 
